adc9831ee41a171b0c258ce3d83be346903db042
pdFarnsworth <p.farnsworth5644@gmail.com>
2019-06-13 13:48:12 -0600

    CharacterCreation Update
    Created scene canvas
    Added Label
    Expanded file tree
    Created Button panel
    Created Start Game Button
    Created StartGameButton script
    Created MainMenuButton
    Created MainMenuButton script
    Organized basic layout
    Created Error Message Panel
    Created CopyAlpha script
    Incomplete error display methods

---
+++ Landfall/Assets/Resources/Animations/CharacterCreation/CopyAlpha.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyAlpha : MonoBehaviour
+{
+    Image p;
+    Text t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GetComponentInParent<Image>();
+        t = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color c = new Color(t.color.r, t.color.g, t.color.b, t.color.a);
+        t.color = c;
+    }
+}
---
+++ Landfall/Assets/Resources/Scripts/CharacterCreation/StartGameButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGameButton : MonoBehaviour
+{
+    public GameObject errorPanel;
+    public InputField fNameField, lNameField;
+
+
+    public void StartGame()
+    {
+        // CHECK TO MAKE SURE ALL DATA IS INPUT
+        if (fNameField.text == "")                                                     // No first name
+        {
+            DisplayError("Please enter first name.");
+            return;
+        }
+        if (lNameField.text == "")                                                     // No last name
+        {
+            DisplayError("Please enter last name.");
+        }
+
+        // Find Player
+        PlayerData pd = FindObjectOfType<Player>().data;
+        pd.fName = fNameField.text;
+
+
+        // Load Scene
+        SceneManager.LoadScene(2);
+    }
+
+
+    private void DisplayError(string s)
+    {
+        errorPanel.SetActive(true);
+    }
+
+    IEnumerator errorPause(Animator a)
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
