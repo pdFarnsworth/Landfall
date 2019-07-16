9fcec69c6e25de984cd202414ff680362a1e4aed
pdFarnsworth <p.farnsworth5644@gmail.com>
2019-07-16 12:33:17 -0600

    Character Creation Update
    Brought UMA into scene
    Added spawner location
    created render texture
    positioned camera
    Added gender selection
    Added height slider panel

---
+++ Landfall/Assets/Resources/Scripts/CharacterCreation/AvatarCreationScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UMA;
+using UMA.CharacterSystem;
+
+public class AvatarCreationScript : MonoBehaviour
+{
+    public DynamicCharacterAvatar avatar;
+    private Dictionary<string, DnaSetter> myDNA;
+
+    public Slider heightSlider;
+    public Slider weightSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //avatar = FindObjectOfType<DynamicCharacterAvatar>();
+    }
+    void OnEnable()
+    {
+        avatar.CharacterUpdated.AddListener(Updated);
+        heightSlider.onValueChanged.AddListener(AdjustHeight);
+    }
+    void OnDisable()
+    {
+        avatar.CharacterUpdated.RemoveListener(Updated);
+        heightSlider.onValueChanged.RemoveListener(AdjustHeight);
+    }
+    void Updated(UMAData data)
+    {
+        myDNA = avatar.GetDNA();
+        heightSlider.value = myDNA["height"].Get();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeGender(string s)
+    {
+        if (s == "Male" && avatar.activeRace.name != "HumanMaleDCS")
+        {
+            avatar.ChangeRace("HumanMaleDCS");
+        }
+        if (s == "Female" && avatar.activeRace.name != "HumanFemaleDCS")
+        {
+            avatar.ChangeRace("HumanFemaleDCS");
+        }
+    }
+
+    public void AdjustHeight(float val)
+    {
+        myDNA["height"].Set(val);
+        avatar.BuildCharacter();
+    }
+    public void AdjustWeight()
+    { }
+
+    public void ChangeSkinColor(Color col)
+    {
+        avatar.SetColor("Skin", col);
+        avatar.UpdateColors(true);
+    }
+}
