5aa04dcfe04b5874aadc4643d04ea2962c6be431
pdFarnsworth <p.farnsworth5644@gmail.com>
2019-07-17 11:07:28 -0600

    CharacterCreation UI Update
    Added section for profession

--- Landfall/Assets/Resources/Scripts/Player/Player.cs
+++ Landfall/Assets/Resources/Scripts/Player/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public PlayerData data;
+    public PlayerData data = new PlayerData();
     // Start is called before the first frame update
     void Start()
     {
